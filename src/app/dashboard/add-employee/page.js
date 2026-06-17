f7dd122669c08f928d958fb694442c9be7183389
Arturo M. Cabaraban Jr. <amcabaraban@kitrol.local>
2026-06-17 22:06:39 +0800

    Remove Add Employee Button

--- src/app/dashboard/add-employee/page.js
+++ src/app/dashboard/add-employee/page.js
@@ -95,10 +95,7 @@ export default function AddEmployeePage() {
                 <div className="flex-1 w-full">
                 <header className="bg-white shadow-sm p-4 flex justify-between items-center">
                     <h2 className="text-lg font-semibold text-gray-700">➕ Add New Employee</h2>
-                    <button onClick={() => router.push('/dashboard/add-employee')}
-                        className="bg-blue-600 text-white px-4 py-2 rounded text-sm hover:bg-blue-700">
-                        <span>+ Add Employee</span>
-                    </button>
+                    
                 </header>
 
                 <main className="p-6">
